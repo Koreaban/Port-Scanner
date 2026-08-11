592e6259150bb684a1a3531f5ac47720fbd30b45
Koreaban <sashateslayyy@gmail.com>
2026-08-11 16:16:13 +0200

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -4,50 +4,22 @@ import json
 import os
 import sys
 from concurrent.futures import ThreadPoolExecutor
-
+from port import all_scan, range_scan, one_scan
+from text import get_interface
 ports = [21, 22, 25, 80, 110, 443, 8080]
 ports_secret = ["21 - FTP, 22 - SSH, 25 - SMTP, 80 - HTTP, 110 - POP, 443 - HTTPS, 8080 - HTTP-Alternate"]
 
-hello = r"""
+hello, i_can = get_interface()
 
-===============================================================
-                   Python Port Scanner v1.0
-===============================================================
- ____            _      _____
-|  _ \ ___  _ __| |_   / ____| ___ __ _ _ __  _ __   ___ _ __
-| |_) / _ \| '__| __| | (___  / __/ _` | '_ \| '_ \ / _ \ '__|
-|  __/ (_) | |  | |_   \___ \| (_| (_| | | | | | | |  __/ |
-|_|   \___/|_|   \__|  |____/ \___\__,_|_| |_|_| |_|\___|_|
-
-
-[1] Start Scan
-[99] Help
-[0] Exit
-================================================================
-"""
-
-i_can = """
-================ PORT SCANNER =================
-
-Features:
-
-[✓] Scan domains and IP addresses
-[✓] Scan a single port
-[✓] Scan all predefined ports
-[✓] Scan a custom port range
-[✓] Detect open and closed ports
-[✓] Identify common services
-[✓] Measure response time
-[✓] Grab HTTP/service banners
-[✓] Save results to JSON
-[✓] Multithreaded scanning
-
-===============================================
-"""
 def hello_function(hello, i_can):
     while True:
         print(hello)
-        final = int(input("Type the nubmer: "))
+        try:
+            final = int(input("Type the nubmer: "))
+
+        except ValueError:
+            print("Please enter a valid number.")
+            continue
 
         if final == 1:
             while True:
@@ -71,7 +43,13 @@ def hello_function(hello, i_can):
 ip, target = hello_function(hello, i_can)
 
 print(f"{ports_secret}\nChoose the ports or write 'all' for scanning all of ports, you also can write diaposon whith '-'")
-port = input("Enter the port/all or diaposon wiht '-'  ports of target site: ")
+while True:
+    port = input("Enter the port/all or diaposon wiht '-'  ports of target site: ")
+    if port in ["all"] or "-" in port or port.isdigit():
+        break
+    else:
+        print("Please enter 'all' or a valid port number or a range with '-'")
+        continue
 
 def scan_port(ip, target, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -94,7 +72,6 @@ def scan_port(ip, target, port):
         servbyport = "Unknown"
 
     if result == 0:
-        print(f"{port} - {servbyport} is \033[92mopen\033[0m, connectoin time: {time_result:.2f} ms")
         status = "Open"
 
         if port in (80, 8080):
@@ -109,22 +86,16 @@ def scan_port(ip, target, port):
 
             http_banner = response.decode(errors="ignore")
 
-            print(http_banner)
-
         else:
             try:
                 banner = s.recv(1024)
 
                 service_banner = banner.decode(errors="ignore")
 
-                print(service_banner)
             except socket.timeout:
-                print("No banner received")
+                pass
 
     else:
-        print(f"{port} {servbyport} - is \033[31mclose\033[0m")
-        print(f"Error code - {result}")
-        print(f"Description: {os.strerror(result)}")
         status = "Close"
 
     result_json = {
@@ -145,72 +116,12 @@ def scan_port(ip, target, port):
 
 results = []
 futures = []
+
 if port == 'all':
-    with ThreadPoolExecutor(max_workers=7) as executor:
-        for p in ports:
-            futures.append(executor.submit(scan_port, ip, target, p))
-
-        for future in futures:
-            results.append(future.result())
-
-            results.sort(key=lambda x:["port"])
-
-        for result in results:
-                   print(
-                       f"{result['port']} {result['port service']} - "
-                       f"{result['status']} "
-                       f"({result['response_time_ms']} ms)"
-                   )
-        with open("scan.json", "w") as file:
-            json.dump(results, file, indent=4)
+    all_scan(ip, target, ports, scan_port),
 
 elif "-" in port:
-    try:
-        start, end = port.split("-")
-
-        start = int(start)
-        end = int(end)
-        
-        if start > end:
-            print("Start port must be less than end port")
-
-        else:
-            with ThreadPoolExecutor(max_workers=10) as executor:
-
-               for p in range(start, end +1):
-                  futures.append(executor.submit(scan_port, ip, target, p))
-
-               for future in futures:
-                   results.append(future.result())
-
-                   results.sort(key=lambda x: x["port"])
-               for result in results:
-                   print(
-                       f"{result['port']} {result['port service']} - "
-                       f"{result['status']} "
-                       f"({result['response_time_ms']} ms)"
-                   )
-               with open("scan.json", "w") as file:
-                   json.dump(results, file, indent=4)
-
-    except ValueError:
-        print("Incorrect range")
+    range_scan(ip, target, port, scan_port)
 
 else:
-    try:
-        port = int(port)
-
-        result = scan_port(ip, target, port)
-
-        results.append(result)
-
-        for result in results:
-            print(
-                f"{result['port']} {result['port service']} - "
-                f"{result['status']} "
-                f"({result['response_time_ms']} ms)"
-            )
-        with open("scan.json", "w") as file:
-            json.dump(results, file, indent=4)
-    except ValueError:
-        print(f"{port} is incorrect")
+    one_scan(ip, target, port, scan_port)
